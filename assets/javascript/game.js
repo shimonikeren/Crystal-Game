3cee1d71c91c7d42741e23fa04f731c23f61d42d
unknown <kerenshimoni6@gmail.com>
2018-02-12 17:43:26 -0800

    styling fun!

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,13 @@
+$( "#winImageHolder" ).ready(function() {
+  $( "#winPic").hide();
+  $( "#losePic").hide();
+});
+
+//document ready function for jQuery
 $(document).ready(function(){ 
 
-//declare variables 
+//declare variables
+var targetNumber = Math.floor((Math.random() * 101) + 19);  // returns a number between 19 and 120. this will happen upon loading the document
 var total = 0;
 var wins = 1;
 var loses = 1;
@@ -8,7 +15,8 @@ var crys1Value = Math.floor((Math.random() * 12) + 1); //returns a number betwee
 var crys2Value = Math.floor((Math.random() * 12) + 1);
 var crys3Value = Math.floor((Math.random() * 12) + 1);
 var crys4Value = Math.floor((Math.random() * 12) + 1);
-var targetNumber = Math.floor((Math.random() * 120) + 19);  // returns a number between 19 and 120
+
+
 
 //when game starts, generate random # between 19-120
 //display this number in HTML #targetNumber		
@@ -18,7 +26,7 @@ var targetNumber = Math.floor((Math.random() * 120) + 19);  // returns a number 
 
 //create a reset function
 	function reset() {
-	targetNumber = Math.floor((Math.random() * 120) + 19);
+	targetNumber = Math.floor((Math.random() * 101) + 19);
 	$("#targetNumber").html("Target Number: " + targetNumber);
 	crys1Value = Math.floor((Math.random() * 12) + 1); 
 	crys2Value = Math.floor((Math.random() * 12) + 1);
@@ -28,80 +36,79 @@ var targetNumber = Math.floor((Math.random() * 120) + 19);  // returns a number 
 	$("#yourTotal").html("Your Total: 0");
 	};
 
-//assign each crytal image a random value between 1-12
-//console log values to keep track of values
+
+//create function for when user wins
+function winner (){
+	//alert("You won! Congrats!");
+  $( "#winPic" ).fadeIn(500);
+  $( "#winPic" ).fadeOut(2000);
+	$("#wins").html("Wins: " + wins++);
+	reset();
+}
+
+//create function for when user loses
+function loser (){
+	// alert("You lost! Sorry, try again!");
+	$( "#losePic" ).fadeIn(500);
+  	$( "#losePic" ).fadeOut(2000);
+	$("#loses").html("Loses: " + loses++);
+	reset();
+}
+
+//function for when each crystal when clicked
 //as each crystal is clicked, add the value of that crystal to yourTotal 
 //display values in game by ADDING them to yourTotal 
-//compare total to targetNumber to determine if user wins or looses 
+//compare total to targetNumber to determine if user wins or looses, then call reset function 
 	$("#crystal1").click(function(){
-		console.log("Crystal 1 Value: " + crys1Value);
 		total = total + crys1Value;
-		console.log("New total: " + total);
 		$("#yourTotal").html("Your Total Score: " + total);
 		if (total === targetNumber) {
-		$("#wins").html("Wins: " + wins++);
-		reset();
-		 }
+		winner ();
+		}
 		else if (total > targetNumber) {
-		$("#loses").html("Loses: " + loses++); 
-		reset();}
+		loser(); 
+		}
 		else {}
 	});
 
 	$("#crystal2").click(function(){
-		console.log("Crystal 2 Value: " + crys2Value);
 		total = total + crys2Value;
-		console.log("New total: " + total);
 		$("#yourTotal").html("Your Total Score: " + total);
 		if (total === targetNumber) {
-		$("#wins").html("Wins: " + wins++);
-		reset();
-		 }
+		winner ();
+		}
 		else if (total > targetNumber) {
-		$("#loses").html("Loses: " + loses++); 
-		reset();}
+		loser(); 
+		}
 		else {}
 
 	});
 	
 	$("#crystal3").click(function(){
-		console.log("Crystal 3 Value: " + crys3Value);
 		total = total + crys3Value;
-		console.log("New total: " + total);
 		$("#yourTotal").html("Your Total Score: " + total);
 		if (total === targetNumber) {
-		$("#wins").html("Wins: " + wins++);
-		reset();
-		 }
+		winner ();
+		}
 		else if (total > targetNumber) {
-		$("#loses").html("Loses: " + loses++); 
-		reset();}
+		loser(); 
+		}
 		else {}
 
 	});
 	
 	$("#crystal4").click(function(){
-		console.log("Crystal 4 Value: " + crys4Value);
 		total = total + crys4Value;
-		console.log("New total: " + total);
 		$("#yourTotal").html("Your Total Score: " + total);
 		if (total === targetNumber) {
-		$("#wins").html("Wins: " + wins++);
-		reset();
-		 }
+		winner ();
+		}
 		else if (total > targetNumber) {
-		$("#loses").html("Loses: " + loses++); 
-		reset();}
+		loser(); 
+		}
 		else {}
 
 	});
-
-
-
-
-
-
-
 
 
 });
